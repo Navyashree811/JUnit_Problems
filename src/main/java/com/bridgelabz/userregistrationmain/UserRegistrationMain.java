e75df3b84647dd912b52a3e9a2f69d32372059db
hp-pc <navyashree811@gmail.com>
2022-08-28 22:18:36 +0530

    Added : Test Validate multiple entry for the Email address

--- src/main/java/com/bridgelabz/userregistrationmain/UserRegistrationMain.java
+++ src/main/java/com/bridgelabz/userregistrationmain/UserRegistrationMain.java
@@ -1,38 +1,93 @@
 package com.bridgelabz.userregistrationmain;
 
+import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class UserRegistrationMain {
 
-	public static final String NAME_VALIDATOR = "^[A-Z]{1}[a-z]{2,}";
-	public static final String NUMBER_VALIDATOR = "^[0-9]{1,3} [0-9]{10}$";
-	public static final String EMAIL_VALIDATIOR = "^[a-zA-Z][a-zA-Z0-9_.]*@[a-zA-Z0-9]+([.][a-zA-Z]+)+$";
-	public static final String PASSWORD_VALIDATION = "(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[@#$%^&-+=()]).{8,}";
+	private final String NAME_VALIDATOR = "^[A-Z]{1}[a-z]{2,}$";
+	private final String LAST_NAME_VALIDATOR = "^[A-Z]{1}[a-z]{2,}$";
+	private final String EMAIL_VALIDATOR = "^[a-zA-Z][a-zA-Z0-9_.]*@[a-zA-Z0-9]+([.][a-zA-Z]+)+$";
+	private final String MOBILE_NUM_VALIDATOR = "^[0-9]{1,3} [0-9]{10}$";
+	private final String PASSWORD_RULE1_VALIDATOR = "[a-z]{8,}";
+	private final String PASSWORD_RULE2_VALIDATOR = "[A-Z]{1,}[a-z]{8,}";
+	private final String PASSWORD_RULE3_VALIDATOR = "[a-z A-z 0-9]{8,}";
+	private final String PASSWORD_RULE4_VALIDATOR = "(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[@#$%^&-+=()]).{8,}";
+	static Scanner sc = new Scanner(System.in);
 
-	public boolean firstNameValidator(String firstName) {
-
-		return Pattern.matches(NAME_VALIDATOR, firstName);
+	public static boolean firstNameValidator() {
+		UserRegistrationMain uservalidate = new UserRegistrationMain();
+		System.out.println("Enter the first name: ");
+		String fName = UserRegistrationMain.sc.nextLine();
+		return Pattern.matches(uservalidate.NAME_VALIDATOR, fName);
 	}
 
-	public boolean lastNameValidator(String lastName) {
-
-		return Pattern.matches(NAME_VALIDATOR, lastName);
+	public static boolean lastNameValidator() {
+		UserRegistrationMain uservalidate = new UserRegistrationMain();
+		System.out.println("Enter the Last name: ");
+		String lName = UserRegistrationMain.sc.nextLine();
+		return Pattern.matches(uservalidate.LAST_NAME_VALIDATOR, lName);
 	}
 
-	public boolean mobileNumberValidator(String mobNumber) {
-		return Pattern.matches(NUMBER_VALIDATOR, mobNumber);
+	public static boolean emailValidator(String email2Test) {
+		UserRegistrationMain uservalidate = new UserRegistrationMain();
+		System.out.println("Enter the Email address: ");
+		String email = UserRegistrationMain.sc.nextLine();
+		return Pattern.matches(uservalidate.EMAIL_VALIDATOR, email);
 	}
 
-	public boolean emailValidator(String email) {
-		return Pattern.matches(EMAIL_VALIDATIOR, email);
+	public static boolean mobNumValidator() {
+		UserRegistrationMain uservalidate = new UserRegistrationMain();
+		System.out.println("Enter the Mobile number: ");
+		String mobNum = UserRegistrationMain.sc.nextLine();
+		return Pattern.matches(uservalidate.MOBILE_NUM_VALIDATOR, mobNum);
 	}
 
-	public boolean passwordValidation(String password) {
-		return Pattern.matches(PASSWORD_VALIDATION, password);
+	public static boolean password1Validator() {
+		UserRegistrationMain uservalidate = new UserRegistrationMain();
+		System.out.println("Enter the Password: ");
+		String password1 = UserRegistrationMain.sc.nextLine();
+		return Pattern.matches(uservalidate.PASSWORD_RULE1_VALIDATOR, password1);
 	}
 
-	public int sum(int a, int b) {
-		return a + b;
+	public static boolean password2Validator() {
+		UserRegistrationMain uservalidate = new UserRegistrationMain();
+		System.out.println("Enter the Password 2: ");
+		String password2 = UserRegistrationMain.sc.nextLine();
+		return Pattern.matches(uservalidate.PASSWORD_RULE2_VALIDATOR, password2);
 	}
 
+	public static boolean password3Validator() {
+		UserRegistrationMain uservalidate = new UserRegistrationMain();
+		System.out.println("Enter the Password 3: ");
+		String password3 = UserRegistrationMain.sc.nextLine();
+		return Pattern.matches(uservalidate.PASSWORD_RULE3_VALIDATOR, password3);
+	}
+
+	public static boolean password4Validator() {
+		UserRegistrationMain uservalidate = new UserRegistrationMain();
+		System.out.println("Enter the Password 4: ");
+		String password4 = UserRegistrationMain.sc.nextLine();
+		return Pattern.matches(uservalidate.PASSWORD_RULE4_VALIDATOR, password4);
+	}
+
+	String[] validEmail = { "abc@yahoo.com", "abc-100@yahoo.com", "abc.100@yahoo.com", "abc111@abc.com",
+			"abc-100@abc.net", "abc.100@abc.com.au", "abc@1.com", "abc@gmail.com.com", "abc+100@gmail.com" };
+	String[] invalidEmail = { "abc", "abc@.com.my", "abc123@gmail.a", "abc123@.com", "abc123@.com.com", ".abc@abc.com",
+			"abc()*@gmail.com", "abc@%*.com", "abc..2002@gmail.com", "abc.@gmail.com", "abc@abc@gmail.com",
+			"abc@gmail.com.1a", "abc@gmail.com.aa.au" };
+	{
+		for (String value : validEmail) {
+			System.out.println(value);
+			System.out.println(Pattern.matches(
+					"^[\\w!#$%&’*+/=?`{|}~^-]+(?:\\.[\\w!#$%&’*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$",
+					value));
+		}
+		for (String s : invalidEmail) {
+			System.out.println(s);
+			System.out.println(Pattern.matches(
+					"^[\\w!#$%&’*+/=?`{|}~^-]+(?:\\.[\\w!#$%&’*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$",
+					s));
+		}
+	}
 }
--- src/test/java/com/bridgelabz/userregistrationtest/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/userregistrationtest/UserRegistrationTest.java
@@ -9,81 +9,38 @@ import com.bridgelabz.userregistrationmain.UserRegistrationMain;
 public class UserRegistrationTest {
 
 	@Test
-	public void whenGivenFirstNameShouldValidateAndPassNameValidatorTest() {
-		UserRegistrationMain validator = new UserRegistrationMain();
-		Boolean result = validator.firstNameValidator("Navya");
-		assertEquals("Happy Test Case", true, result);
+	public void givenFirstNameShouldReturnTrue() {
+		assertEquals(true, UserRegistrationMain.firstNameValidator());
 	}
 
 	@Test
-	public void whenGivenLastNameShouldValidateAndPassNameValidatorTest() {
-		UserRegistrationMain validator = new UserRegistrationMain();
-		Boolean result = validator.lastNameValidator("Shree");
-		assertEquals("Happy Test Case", true, result);
+	public void lastNameTestValidator() {
+		assertEquals(true, UserRegistrationMain.lastNameValidator());
+	}
+
+//	@Test
+//	public void emailTestValidator() {
+//		assertEquals(true, UserRegistrationMain.emailValidator());
+//	}
+
+	@Test
+	public void password1Validator() {
+		assertEquals(true, UserRegistrationMain.password1Validator());
 	}
 
 	@Test
-	public void whenEnteredMobileShouldValidateAndPassNumberTest1() {
-		UserRegistrationMain validator = new UserRegistrationMain();
-		Boolean result = validator.mobileNumberValidator("91 8097177781");
-		assertEquals("Happy Test Case", true, result);
+	public void password2Validator() {
+		assertEquals(true, UserRegistrationMain.password2Validator());
 	}
 
 	@Test
-	public void whenEnteredEmailAddressShouldValidateAndPassEmailTest1() {
-		UserRegistrationMain validator = new UserRegistrationMain();
-		Boolean result = validator.emailValidator("navyashree121@gmail.com");
-		assertEquals("Happy Test Case", true, result);
+	public void password3Validator() {
+		assertEquals(true, UserRegistrationMain.password3Validator());
 	}
 
 	@Test
-	public void whenEnteredPasswordShouldValidateAndPassTest() {
-		UserRegistrationMain validator = new UserRegistrationMain();
-		Boolean result = validator.passwordValidation("Anokhi5@co.in");
-		assertEquals("Happy Test Case", true, result);
-	}
-
-	@Test
-	public void whenGivenFirstNameShouldValidateAndPassNameValidatorTest2() {
-		UserRegistrationMain validator = new UserRegistrationMain();
-		Boolean result = validator.firstNameValidator("navya");
-		assertEquals("Sad Test Case", true, result);
-	}
-
-	@Test
-	public void whenGivenLastNameShouldValidateAndPassNameValidatorTest2() {
-		UserRegistrationMain validator = new UserRegistrationMain();
-		Boolean result = validator.lastNameValidator("shree");
-		assertEquals("Sad Test Case", true, result);
-	}
-
-	@Test
-	public void whenEnteredMobileShouldValidateAndPassNumberTest2() {
-		UserRegistrationMain validator = new UserRegistrationMain();
-		Boolean result = validator.mobileNumberValidator("91-8097177781");
-		assertEquals("Sad Test Case", true, result);
-	}
-
-	@Test
-	public void whenEnteredEmailAddressShouldValidateAndPassEmailTest2() {
-		UserRegistrationMain validator = new UserRegistrationMain();
-		Boolean result = validator.emailValidator("anokhi2@.gmail.com");
-		assertEquals("Sad Test Case", true, result);
-	}
-
-	@Test
-	public void whenEnteredPasswordShouldValidateAndPassTest2() {
-		UserRegistrationMain validator = new UserRegistrationMain();
-		Boolean result = validator.passwordValidation("navya123.@");
-		assertEquals("Sad Test Case", true, result);
-	}
-
-	@Test
-	public void test() {
-		UserRegistrationMain obj = new UserRegistrationMain();
-		int actual = obj.sum(20, 10);
-		int expected = 30;
-		assertEquals(expected, actual);
+	public void password4Validator() {
+		assertEquals(true, UserRegistrationMain.password4Validator());
 	}
 
 }
